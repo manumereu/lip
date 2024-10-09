d62bde7c4b5fc544cc4d77b12262dcffb9651172
Docker <docker@example.com>
2024-10-09 15:51:56 +0100

    1

--- basics/recognizer/lib/recognizer.ml
+++ basics/recognizer/lib/recognizer.ml
@@ -1,12 +1,50 @@
-let lang1 _ = failwith ""
+let rec lang1 l = match l with
+    [] -> true
+  | x::t -> (x = '1' || x = '0') && lang1 t
+;;
 
-let lang2 _ = failwith ""
+let rec other l = match l with
+	  [] -> true
+	| x::t -> x = '1' && other t
+;;
 
-let lang3 _ = failwith ""
+let lang2 l = match l with 
+	  [] -> true
+	| x::t -> (x = '0' || x = '1') && other t
+;;
 
-let lang4 _ = failwith ""
+let rec isLastElement l = match l with
+	  [] -> failwith "LISTA VUOTA"
+	| x::[] -> x = '0' 
+	| x::t -> (x = '1' || x = '0') && isLastElement t
+;;
 
-let lang5 _ = failwith ""
+let lang3 l = match l with 
+	  [] -> false
+	| x::t -> (x = '0') && isLastElement t
+
+  let rec verifyList l = match l with
+  [] -> true
+| x::t -> (x = '0'|| x = '1') && verifyList t
+;;
+
+let filtraLista l = l |> List.filter (fun x -> x = '1')
+
+let rec counter l = match filtraLista l with
+  [] -> 0
+| _::[] -> 1
+| _::t -> 1 + counter t
+;;
+
+let lang4 l = counter l = 2 && verifyList l;;
+
+let rec lang5 l = match l with
+	  [] -> true
+	| '0'::'0'::t -> lang5 t
+	| '1'::'1'::t -> lang5 t
+	| _::[] -> false
+	| _::_::_ -> false
+;;
     
 let recognizers = [lang1;lang2;lang3;lang4;lang5]
                   
