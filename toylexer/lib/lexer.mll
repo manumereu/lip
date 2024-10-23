d66dfca2c6e54df91616a0dbda19401ded52f4be
Docker <docker@example.com>
2024-10-23 15:49:43 +0100

    Create dune project in lip/toylexer

--- toylexer/lib/lexer.mll
+++ toylexer/lib/lexer.mll
@@ -7,6 +7,12 @@ let letter = ['a'-'z' 'A'-'Z']
 let chr = ['a'-'z' 'A'-'Z' '0'-'9']
 let id = letter chr*
 let num = ['0'-'9']|['1'-'9']['0'-'9']*
+let fisrtCap = ['A'-'Z'] ['A'-'Z''a'-'z''0'-'9']*
+let vowelLower = ['a''e''i''o''u']
+let vowelCap = ['A''E''I''O''U']
+let consonant = ['A'-'Z']#(vowelCap)#(vowelLower)
+
+
 
 rule read_token =
   parse
@@ -19,3 +25,5 @@ rule read_token =
   | id { ID (Lexing.lexeme lexbuf) }
   | num { CONST (Lexing.lexeme lexbuf) }    
   | eof { EOF }
+  | fisrtCap {ATOK}
+  | vowelLower* {BTOK}
--- toylexer/lib/main.ml
+++ toylexer/lib/main.ml
@@ -24,4 +24,15 @@ let string_of_frequencies fl =
   List.fold_left (fun s (t,n) -> s ^ ((string_of_token t) ^ " -> " ^ string_of_int n ^ "\n")) "" fl
 
 (* frequency : int -> 'a list -> ('a * int) list *)
-let frequency _ _ = failwith("TODO")
+
+let occorrenze a l = let m = l |> List.filter(fun x -> x=a) |> List.length in m + 1;;
+
+let rimuovi a l = l |> List.filter(fun x -> x<>a);;
+
+let frequency n l = 
+  let rec listaNonOrdinata n t = if n>0 then 
+  match t with
+      [] -> []
+     | x::t -> [(x, occorrenze x t)] @ listaNonOrdinata (n-1) (rimuovi x t)
+    else 
+      [] in List.sort(fun(_,a)(_,b) -> if(a<b) then 1 else -1) (listaNonOrdinata n l);;
