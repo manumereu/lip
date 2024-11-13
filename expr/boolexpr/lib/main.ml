e755c8a06a74b018f0df2bb6994caffd4c01b73d
Docker <docker@example.com>
2024-11-13 15:24:39 +0000

    Boolexpr da terminare

--- expr/boolexpr/lib/main.ml
+++ expr/boolexpr/lib/main.ml
@@ -17,7 +17,7 @@ exception NoRuleApplies
 let rec trace1 = function
     If(True,e1,_) -> e1
   | If(False,_,e2) -> e2
-  | If(_,_,_) -> failwith "TODO"
+  | If(e0,e1,e2) -> If(trace1 e0,e1,e2)
   | _ -> raise NoRuleApplies
 
 let rec trace e = try
@@ -29,4 +29,4 @@ let rec trace e = try
 let rec eval = function
     True -> true
   | False -> false
-  | If(_,_,_) -> failwith "TODO"
+  | If(e0,e1,e2) -> if eval e0 then eval e1 else eval e2
--- expr/boolexpr/test/boolexpr.ml
+++ expr/boolexpr/test/boolexpr.ml
@@ -5,7 +5,13 @@ let test_eval expr exp_result =
 
 (* ### Unit tests for task 4 *)
 
-let%test "test_eval_1" = failwith "TODO"
+let%test "test_eval_1" = test_eval "false" false
+let%test "test_eval_1" = test_eval "true" true
+let%test "test_eval_1" = test_eval "if true then false else true" false
+let%test "test_eval_1" = test_eval "if false then false else true" true
+let%test "test_eval_1" = test_eval "if true then (if true then false else true) else (if true then true else false)" false
+let%test "test_eval_1" = test_eval "if (if false then false else false) then (if false then true else false) else (if true then false else true)" false
+let%test "test_eval_1" = test_eval "if (if (if false then false else false) then (if false then true else false) else (if true then false else true)) then (if false then true else false) else (if true then false else true)" false
 
 (* ### Unit tests for task 5 *)
 
